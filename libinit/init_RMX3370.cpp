41ae1e0c68cac1b0f6c780b6e9b8cdb1a2d19e91
klozz <klozz@TheXPerienceProject.org>
2022-02-26 20:15:12 -0600

    RMX3370: Add 8 & 12GB RAM support
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- libinit/init_RMX3370.cpp
+++ libinit/init_RMX3370.cpp
@@ -38,6 +38,7 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
@@ -62,6 +63,28 @@ void property_override(char const prop[], char const value[], bool add = true) {
         __system_property_update(pi, value, strlen(value));
     else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void load_dalvik_properties() {
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+    if (sys.totalram < 8192ull * 1024 * 1024) {
+        // 8GB ram
+        property_override("dalvik.vm.heapstartsize", "24m");
+        property_override("dalvik.vm.heapgrowthlimit", "256");
+        property_override("dalvik.vm.heapsize", "512m");
+        property_override("dalvik.vm.heapmaxfree", "48m");
+    } else {
+        // 12GB RAM
+        property_override("dalvik.vm.heapstartsize", "24m");
+        property_override("dalvik.vm.heapgrowthlimit", "384m");
+        property_override("dalvik.vm.heapsize", "512m");
+        property_override("dalvik.vm.heapmaxfree", "56m");
+    }
+
+    property_override("dalvik.vm.heaptargetutilization", "0.5");
+    property_override("dalvik.vm.heapminfree", "8m");
 }
 
 void set_device_props(const std::string fingerprint, const std::string description,
@@ -101,6 +124,8 @@ void vendor_load_properties() {
     string region = android::base::GetProperty("ro.boot.hwc", "");
 
     set_device_props(fp, fp_desc, "Realme", "RMX3370", "Realme GT Neo 2 5G");
+
+    load_dalvik_properties();
 
 //  SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");

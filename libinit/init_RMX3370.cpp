9d8c25037f7c7b300e310f31ee513a8e71c9fc7c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2022-02-26 20:19:35 -0600

    RMX3370: Disable oem unlock
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- libinit/init_RMX3370.cpp
+++ libinit/init_RMX3370.cpp
@@ -129,4 +129,5 @@ void vendor_load_properties() {
 
 //  SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.oem_unlock_supported", "0");
 }
